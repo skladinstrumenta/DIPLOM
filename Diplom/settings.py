ea18a6979e7875ef31cd07970e7831ee187087ce
Alexandr <skladinstrumenta@gmail.com>
2023-01-07 11:58:27 +0200

    modified 07.01

--- Diplom/settings.py
+++ Diplom/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'Latrello.middlewares.AuthenticationMiddleware',
+    'Latrello.middlewares.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
--- Latrello/middlewares.py
+++ Latrello/middlewares.py
@@ -1,19 +1,12 @@
-from django.contrib import auth
 from django.utils.deprecation import MiddlewareMixin
-from django.utils.functional import SimpleLazyObject
 
-
-# def get_user(request):
-#     if not hasattr(request, '_cached_user'):
-#         request._cached_user = auth.get_user(request)
-#     return request._cached_user
-
-
-# class AuthenticationMiddleware(MiddlewareMixin):
-#     def process_request(self, request):
-#         if request.user.is_authenticated:
-#             session_count = request.session.get('session_count', 1)
-#             request.session['session_count'] = session_count + 1
-#             if not request.user.is_superuser:
-#                 request.session.set_expiry(60)
-#         return request.user
+class AuthenticationMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        session_count = 0
+        if request.user.is_authenticated:
+            session_count = request.session.get('session_count')
+            if request.method == 'GET':
+                session_count += 1
+            if not request.user.is_superuser:
+                request.session.set_expiry(60)
+        request.session.update({'session_count': session_count})
